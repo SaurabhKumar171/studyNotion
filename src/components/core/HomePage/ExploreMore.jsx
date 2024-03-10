43b520658d2fe1d05861e946ee1e000aea311f4d
saurabh <saurabhkumarjha416@gmail.com>
2024-03-10 21:57:14 +0530

    Add course component - api creation , slices creation

--- src/components/core/HomePage/ExploreMore.jsx
+++ src/components/core/HomePage/ExploreMore.jsx
@@ -26,7 +26,7 @@ const ExploreMore = () => {
   };
 
   return (
-    <div className="absolute">
+    <div className="relative mb-52 w-full">
       {/* Explore more section */}
       <div>
         <div className="text-4xl font-semibold xs:text-center my-10">
@@ -59,7 +59,7 @@ const ExploreMore = () => {
       <div className="hidden lg:block lg:h-[200px]"></div>
 
       {/* Cards Group */}
-      <div className="lg:absolute gap-10 justify-center lg:gap-0 flex lg:justify-between flex-wrap w-full lg:bottom-[0] lg:left-[50%] lg:translate-x-[-50%] lg:translate-y-[50%] text-black lg:mb-0 mb-7 lg:px-0 px-3">
+      <div className="absolute gap-10 justify-center lg:gap-0 flex lg:justify-between flex-wrap w-full lg:bottom-[0] lg:left-[50%] lg:translate-x-[-50%] lg:translate-y-[50%] text-black lg:mb-0 mb-7 lg:px-0 px-3">
         {courses.map((ele, index) => {
           return (
             <CourseCard
