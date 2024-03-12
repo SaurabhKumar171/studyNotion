5563c5e032cd4ba45bd05f5b1e9827c2bae1a06d
saurabh <saurabhkumarjha416@gmail.com>
2024-03-12 23:40:06 +0530

    Developed add course Component

--- server/controllers/Course.js
+++ server/controllers/Course.js
@@ -14,14 +14,14 @@ exports.createCourse = async (req, res) => {
 			courseDescription,
 			whatYouWillLearn,
 			price,
-			tag,
+			// tag,
 			category,
 			status,
 			instructions,
 		} = req.body;
 
 		// Get thumbnail image from request files
-		const thumbnail = req.files.thumbnailImage;
+		// const thumbnail = req.files.thumbnailImage;
 
 		// Check if any of the required fields are missing
 		if (
@@ -29,8 +29,8 @@ exports.createCourse = async (req, res) => {
 			!courseDescription ||
 			!whatYouWillLearn ||
 			!price ||
-			!tag ||
-			!thumbnail ||
+			// !tag ||
+			// !thumbnail ||
 			!category
 		) {
 			return res.status(400).json({
@@ -62,11 +62,11 @@ exports.createCourse = async (req, res) => {
 			});
 		}
 		// Upload the Thumbnail to Cloudinary
-		const thumbnailImage = await uploadImageToCloudinary(
-			thumbnail,
-			process.env.FOLDER_NAME
-		);
-		console.log(thumbnailImage);
+		// const thumbnailImage = await uploadImageToCloudinary(
+		// 	 thumbnail,
+		// 	process.env.FOLDER_NAME
+		// );
+		// console.log(thumbnailImage);
 		// Create a new course with the given details
 		const newCourse = await Course.create({
 			courseName,
@@ -74,9 +74,9 @@ exports.createCourse = async (req, res) => {
 			instructor: instructorDetails._id,
 			whatYouWillLearn: whatYouWillLearn,
 			price,
-			tag: tag,
+			// tag: tag,
 			category: categoryDetails._id,
-			thumbnail: thumbnailImage.secure_url,
+			// thumbnail: thumbnailImage.secure_url,
 			status: status,
 			instructions: instructions,
 		});
---
+++ src/components/core/Dashboard/AddCourse/CourseInformation/ChipInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChipInput = () => {
+  return (
+    <div>ChipInput</div>
+  )
+}
+
+export default ChipInput
